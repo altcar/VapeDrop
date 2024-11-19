310a034017de50ef06d599bb97bbfb72d814bdfe
You <hi@example.com>
2024-11-19 19:23:46 +0000

    change

--- src/app/component/qrscanner.js
+++ src/app/component/qrscanner.js
@@ -1,12 +1,24 @@
 "use client"
 import { useState, useEffect } from 'react';
-// import dynamic from 'next/dynamic';
 import { QrReader } from 'react-qr-reader';
 
-// const QrReader = dynamic(() => import('react-qr-reader'), { ssr: false });
-
-const QrScanner = ({ onScanResult, deviceid }) => {
+const QrScanner = ({ onScanResult }) => {
   const [result, setResult] = useState('');
+  const [devices, setDevices] = useState([]);
+  const [selectedDeviceId, setSelectedDeviceId] = useState('');
+
+  useEffect(() => {
+    const getDevices = async () => {
+      const devices = await navigator.mediaDevices.enumerateDevices();
+      const videoDevices = devices.filter(device => device.kind === 'videoinput');
+      setDevices(videoDevices);
+      if (videoDevices.length > 0) {
+        setSelectedDeviceId(videoDevices[0].deviceId);
+      }
+    };
+
+    getDevices();
+  }, []);
 
   const handleScan = (data) => {
     if (data) {
@@ -23,15 +35,20 @@ const QrScanner = ({ onScanResult, deviceid }) => {
 
   return (
     <div>
-      
+      <select onChange={(e) => setSelectedDeviceId(e.target.value)} value={selectedDeviceId}>
+        {devices.map((device, index) => (
+          <option key={index} value={device.deviceId}>
+            {device.label || `Camera ${index + 1}`}
+          </option>
+        ))}
+      </select>
       <QrReader
         delay={300}
         onError={handleError}
         onScan={handleScan}
-        faceMode="user"
-        constraints={{ video: { deviceId: deviceid ? { exact: deviceid } : undefined } ,    facingMode: { exact: "environment" }} }
+        constraints={{ video: { deviceId: selectedDeviceId ? { exact: selectedDeviceId } : undefined } }}
       />
-      {/* <p>{result}</p> */}
+      <p>{result}</p>
     </div>
   );
 };
