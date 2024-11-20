fe7533d8fbbc7e4665bf2bd92087447f5d3dddfa
You <hi@example.com>
2024-11-20 00:42:33 +0000

    change

--- src/app/component/qrscanner.js
+++ src/app/component/qrscanner.js
@@ -17,13 +17,38 @@ const QrScanner = ({ onScanResult, deviceid }) => {
     }
   };
 
+  const [devices, setDevices] = useState([]);
+  const [selectedDeviceId, setSelectedDeviceId] = useState('');
+
+  useEffect(() => {
+    function detectcamera() {
+      navigator.mediaDevices.enumerateDevices().then((mediaDevices) => {
+        const videoDevices = mediaDevices.filter(({ kind }) => kind === 'videoinput');
+        setDevices(videoDevices);
+        if (videoDevices.length > 0) {
+          const backCamera = videoDevices.find(device => device.label.toLowerCase().includes('back') || device.label.toLowerCase().includes('rear'));
+          setSelectedDeviceId(backCamera ? backCamera.deviceId : videoDevices[0].deviceId);
+        }
+      });
+    }
+
+  }, []);
+
   const handleError = (err) => {
     console.error(err);
   };
-
+  useEffect(() => {
+    console.log(deviceid);
+  }, [deviceid]);
   return (
     <div>
-
+      <select onChange={(e) => setSelectedDeviceId(e.target.value)} value={selectedDeviceId}>
+          {devices.map((device, key) => (
+            <option value={device.deviceId} key={key}>
+              {device.label || `Device ${key + 1}`}
+            </option>
+          ))}
+        </select> <p>Scan Result: {scanResult}</p>
       <QrReader
         key={deviceid} // Add key prop to force re-render
         delay={300}
